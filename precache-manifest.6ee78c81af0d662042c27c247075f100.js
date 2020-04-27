61d5a4f86a587f60c0747caae988a8764394853e
James Helms <helms.james@gmail.com>
2020-04-27 16:10:10 -0500

    Updates

---
+++ precache-manifest.6ee78c81af0d662042c27c247075f100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7497e929847f50303c4d680d41ed5e",
+    "url": "/helmsjames/User-Directory/index.html"
+  },
+  {
+    "revision": "eb292642682faae3e157",
+    "url": "/helmsjames/User-Directory/static/css/main.38c628ad.chunk.css"
+  },
+  {
+    "revision": "b7a07d82dba02adbbc20",
+    "url": "/helmsjames/User-Directory/static/js/2.7258b704.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/helmsjames/User-Directory/static/js/2.7258b704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb292642682faae3e157",
+    "url": "/helmsjames/User-Directory/static/js/main.f503d6f5.chunk.js"
+  },
+  {
+    "revision": "4bb43b20fb0fecd67c2d",
+    "url": "/helmsjames/User-Directory/static/js/runtime-main.9f430414.js"
+  }
+]);
