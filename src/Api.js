d6cd0e7e0da6beb96fcf3b4bb549ca5da98bdae8
James Helms <helms.james@gmail.com>
2020-05-05 11:15:47 -0500

    final commit

--- src/Api.js
+++ src/Api.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-// function getUsers() {
-//   console.log('get users called')
-//     return axios.get("https://randomuser.me/api/?results=200&nat=us");  
-// }
-
 const getUsers = async () => {
   return axios.get("https://randomuser.me/api/?results=200&nat=us")
   .then((res) => {
@@ -16,17 +11,5 @@ const getUsers = async () => {
     console.log('******************8error', error);
   });
 }
-
-
-// function requestUsers() {
-//   return new Promise((resolve, reject) => {
-//       axios.get("https://randomuser.me/api/?results=200&nat=us").then((response) => {
-//           resolve();
-//       }).catch((err) => {
-//           console.log(err);
-//           reject(err);
-//       })
-//   })
-// }
 
 export default getUsers;
